c49f1614350e42cc40ebf4f2c5c9b11ad0241c0b
kjottboller <krestyanova00@gmail.com>
2022-10-15 19:21:31 +0700

    edit readme.md

---
+++ README.md
@@ -0,0 +1,11 @@
+# My Data Science projects
+
+From the [SkillFactory Data Science course] ().
+
+## Проекты
+
+* [Проект 0. Игра: Камень, ножницы, бумага] ()
+* [] ()
+* [] ()
+
+## Ссылки на резюме
---
+++ project 1/game_V1.py
@@ -0,0 +1,19 @@
+## GUESS THE NUMBER??? game
+
+import numpy as np
+
+number = np.random.randint(1, 100) # загадываем число
+count = 0 # количество попыток
+
+while True:
+    count += 1
+    predict_number = int(input("Угадай число от 1 до 100: "))
+    
+    if predict_number > number:
+        print("Число должно быть меньше!")
+    elif predict_number < number:
+        print("Число должно быть больше!")
+    else:
+        print(f"Вы угадали число за {count} попыток! Это число = {number}")
+        break # конец игры и выход из цикла
+        
