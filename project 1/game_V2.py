187c16cc14e6b17d38f0c6eb7810b5c7d1ec866d
kjottboller <krestyanova00@gmail.com>
2022-10-23 21:53:34 +0700

    editing games

--- project 1/game_V2.py
+++ project 1/game_V2.py
@@ -1,35 +1,44 @@
-# Guess the number game (2)
+# Guess the number
 # A computer guessing n choosing a number
+
+print('--------------------')
 
 import numpy as np
 number = np.random.randint(1, 101)
+
 def random_predict(number):
     count_1 = 0
-    mn = 1
-    mx = 101
-    while True:
+    min_num, max_num = 1, 101
+    predict_number = int(np.random.randint(1, 101))
+    
+    while predict_number != number:
         count_1 += 1
-        md = (mn + mx) // 2  
-        if md > number:
-            mx = md
-        elif md < number:
-            mn = md
+        if predict_number > number:
+           max_num = predict_number 
+           predict_number = (max_num + min_num) // 2 
+        elif predict_number < number:
+           min_num = predict_number 
+           predict_number = (max_num + min_num) // 2 
         else:
-            print(f"The computer guessed the number in {count_1} attempts! This is the number {number}.")
-            break 
+            break
     return count_1
-print(f'Number of attempts: {random_predict(number)}')
+print(f"Number of attempts (1): {random_predict(number)}")
 
-print('----------------')
+print('. . .')
 
-def algorithm(random_predict):
-    count_2 = 0
+def own_algorithm(random_predict):
+    count_2 = []
     np.random.seed(1)
-    random_numbers = np.random.randit(1, 101, size = (1000))
-    for number in random_numbers:
+    random_array = np.random.randit(1, 101, size = (1000))
+    
+    for number in random_array:
         count_2.append(int(random_predict(number)))
+    
     score = int(np.mean(count_2))
     print(f"Your algorithm guessing in an average of {score} attempts!")
-    return score
-print(f"Number of attempts: {random_predict(number)}")
-    
+    return(score)
+print(f"Number of attempts (2): {random_predict(10)}")
+
+
+
+print('--------------------')
